c3d80531e8d5f24772d4a2a56d1a3840b54c7805
Bikmeev AF <Shadowfareed@yandex.ru>
2021-02-27 15:54:40 +0300

    Geekbrains JS-1

--- _Lessons/GeekBrains/_JS-1/HW_1-1/script.js
+++ _Lessons/GeekBrains/_JS-1/HW_1-1/script.js
@@ -1,1 +1,28 @@
-console.log('Hello')
+'use strict';
+
+// Задание №1
+function convertTemperature() {
+    let temperatureC = document.getElementById('temperatureC').value;
+    let temperatureF = (9 / 5) * +temperatureC + 32;
+    document.getElementById('temperatureF').innerHTML = ' °' + temperatureF.toFixed(0);
+}
+
+function convertTemperatureAlert() {
+    let temperatureC = document.getElementById('temperatureC').value;
+    let temperatureF = (9 / 5) * +temperatureC + 32;
+    alert('По Фаренгейту: °' + temperatureF.toFixed(0));
+}
+
+// Задание №2
+let userName, admin;
+userName = 'Василий';
+admin = userName;
+document.getElementById('example2').innerHTML = admin;
+console.log('В переменной "admin" значение: ' + admin);
+
+// Задание №3
+document.getElementById('example3').innerHTML = 1000 + '108';
+console.log('При JS выражении: 1000 + "108" сработает конкатенация, поскольку одно из значений типа "string". Проверка: ' + (1000 + '108'));
+
+// Задание №4
+console.log('При атрибуте async по возможности, скрипт выполняется асинхронно - при этом продолжается загрузка страницы. При атрибуте defer скрипт выполняется после полной загрузки страницы.')
